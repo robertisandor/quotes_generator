eddf676e76a5eb861d1a1e0b3f4fec6d6be4f98c
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-05 01:29:01 -0800

    Add EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,3 +73,28 @@ resource "aws_iam_group_policy" "admin_policy" {
         ]
     })
 }
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+    ami           = data.aws_ami.ubuntu.id
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "QuotesApiWeb"
+    }
+}
