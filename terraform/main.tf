70a696a5eb4cabc65bb8f864efd72de2fd457352
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-06 00:01:52 -0800

    Added (attempted) infra to connect rds to ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,7 @@ resource "aws_db_instance" "quotes_generator" {
   allocated_storage    = 5
   engine               = "postgres"
   engine_version       = "15.4"
+  db_name              = "quotes_db"
   username             = "api"
   password             = var.db_password
 #   db_subnet_group_name = aws_db_subnet_group.quotes_generator.name
@@ -36,6 +37,127 @@ resource "aws_db_parameter_group" "quotes_generator" {
     name  = "log_connections"
     value = "1"
   }
+}
+
+resource "aws_vpc" "quotes_main" {
+  cidr_block = "172.31.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true 
+  enable_network_address_usage_metrics = false 
+}
+
+resource "aws_route_table" "quotes_route_table" {
+  vpc_id = aws_vpc.quotes_main.id
+
+  route {
+    cidr_block        = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.quotes_gateway.id
+  }
+}
+
+resource "aws_subnet" "quotes_1" {
+  cidr_block = "172.31.0.0/20"
+  vpc_id     = aws_vpc.quotes_main.id
+}
+
+resource "aws_subnet" "quotes_2" {
+  cidr_block = "172.31.16.0/20"
+  vpc_id     = aws_vpc.quotes_main.id
+}
+
+resource "aws_subnet" "quotes_3" {
+  cidr_block = "172.31.32.0/20"
+  vpc_id     = aws_vpc.quotes_main.id
+}
+
+resource "aws_vpc_dhcp_options" "quotes_dns_resolver" {
+  domain_name_servers  = ["AmazonProvidedDNS"]
+}
+
+resource "aws_internet_gateway" "quotes_gateway" {
+  vpc_id = aws_vpc.quotes_main.id
+}
+
+resource "aws_network_acl" "main" {
+  vpc_id = aws_vpc.quotes_main.id
+  subnet_ids = [aws_subnet.quotes_1.id, aws_subnet.quotes_2.id, aws_subnet.quotes_3.id]
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 1
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"    
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 1
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"    
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
+
+resource "aws_security_group" "rds_ec2_1" {
+  name        = "rds_ec2_1"
+  vpc_id      = aws_vpc.quotes_main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.rds_ec2_1.id
+  description       = "Rule to allow connections from EC2 instances with sg attached"
+  cidr_ipv4         = aws_vpc.quotes_main.cidr_block
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
+
+resource "aws_security_group" "ec2_rds_1" {
+  name        = "ec2_rds_1"
+  vpc_id      = aws_vpc.quotes_main.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.ec2_rds_1.id
+  description       = "Rule to allow connections from EC2 instances with sg attached"
+  cidr_ipv4         = aws_vpc.quotes_main.cidr_block
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
+
+resource "aws_network_interface" "rds_network_interface" {
+  subnet_id       = aws_subnet.quotes_1.id
+  private_ips     = ["172.31.14.150"]
+  security_groups = [aws_security_group.rds_ec2_1.id]
+}
+
+resource "aws_network_interface" "ec2_network_interface" {
+  subnet_id       = aws_subnet.quotes_2.id
+  private_ips     = ["172.31.19.101"]
+  security_groups = [aws_security_group.ec2_rds_1.id]
 }
 
 resource "aws_iam_group" "admin_group_test" {
