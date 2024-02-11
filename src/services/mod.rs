daf71ee54c505ff804d6fdabea948c47a2310bc2
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-11 00:58:54 -0800

    Update connection

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -9,7 +9,7 @@ pub fn establish_connection_pg() -> PgConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let database_connection_string = format!("postgresql://{}:5432", database_url)
-    PgConnection::establish(&database_url)
+    PgConnection::establish(&database_connection_string)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
