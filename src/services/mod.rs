7dedc1dec6534579b267527a5b659759f9818511
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-11 13:23:38 -0800

    Add db username and password as secrets, pass from Github to Dockerfile to source code

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -8,7 +8,9 @@ use std::env;
 pub fn establish_connection_pg() -> PgConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let database_connection_string = format!("postgresql://postgres@{}:5432", database_url);
+    let db_username = env::var("DATABASE_USERNAME").expect("DATABASE_USERNAME must be set");
+    let db_password = env::var("DATABASE_PASSWORD").expect("DATABASE_PASSWORD must be set");
+    let database_connection_string = format!("postgresql://{}:{}@{}:5432", db_username, db_password, database_url);
     PgConnection::establish(&database_connection_string)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_connection_string))
 }
