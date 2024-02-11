8b7ae02886d2f48a1eb613d2f433cc5b032f383b
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-11 01:06:21 -0800

    Fix syntax error

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -8,7 +8,7 @@ use std::env;
 pub fn establish_connection_pg() -> PgConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let database_connection_string = format!("postgresql://{}:5432", database_url)
+    let database_connection_string = format!("postgresql://{}:5432", database_url);
     PgConnection::establish(&database_connection_string)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
