aeb0e80a7f5364a51a166d160076f1be9b7217fd
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-10 19:58:11 -0800

    Add Docker commands to EC2 instance initialization

--- terraform/main.tf
+++ terraform/main.tf
@@ -231,6 +231,19 @@ resource "aws_instance" "web" {
     #!/bin/bash -xe
 
     sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update 
+     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+
     sudo apt-get install -y postgresql-client
     sudo passwd ubuntu <<< "testpassword"
     sudo sed -i -e '/PasswordAuthentication / s/ .*/ yes/' /etc/ssh/sshd_config
