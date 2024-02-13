38f8bb5f90e0023fb4d7e7d31da78599d4640d59
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-12 23:21:42 -0800

    Temporarily remove terraform for testing

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,3 @@
-/*
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.quotes_generator.address
-  sensitive   = true
-}
-
-output "rds_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.quotes_generator.port
-  sensitive   = true
-}
-
-output "rds_username" {
-  description = "RDS instance root username"
-  value       = aws_db_instance.quotes_generator.username
-  sensitive   = true
-}
-*/
 output "ec2_hostname" {
   value	      = aws_instance.web.public_ip
 }
