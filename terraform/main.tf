cd6d840cc9d5182f6d1dfc316ad9e76f40727504
Robert Sandor <robert.i.sandor@gmail.com>
2024-02-27 22:27:52 -0800

    Add terraform remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,78 @@ terraform {
   }
 
   required_version = ">= 0.14.6"
-
+  
+  backend "s3" {
+    bucket         = "quotes-generator"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    kms_key_id     = "alias/quotes-generator-terraform-bucket-key"
+    dynamodb_table = "terraform-state"
+  }
 }
 
 provider "aws" {
   region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "quotes-generator" {
+  bucket = "quotes-generator"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.quotes-generator-terraform-bucket-key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "quotes-generator" {
+  bucket = aws_s3_bucket.quotes-generator.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "quotes-generator" {
+  depends_on = [aws_s3_bucket_ownership_controls.quotes-generator]
+
+  bucket = aws_s3_bucket.quotes-generator.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.quotes-generator.id
+ 
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_kms_key" "quotes-generator-terraform-bucket-key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+ 
+resource "aws_kms_alias" "key-alias" {
+  name          = "alias/quotes-generator-terraform-bucket-key"
+  target_key_id = aws_kms_key.quotes-generator-terraform-bucket-key.key_id
+}
+
+resource "aws_dynamodb_table" "terraform-state" {
+  name           = "terraform-state"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "LockID"
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 resource "aws_db_instance" "quotes_generator" {
@@ -218,6 +285,53 @@ resource "aws_iam_group_policy" "admin_policy" {
                 "Effect": "Allow",
                 "Action": "rds:DescribeDBParameterGroups",
                 "Resource": "arn:aws:rds:*:487577641151:pg:*"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_group_policy" "admin_access_policy" {
+  name  = "admin_access_policy"
+  group = aws_iam_group.admin_group_test.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": "*",
+                "Resource": "*"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_group_policy" "admin_iam_user_change_password_policy" {
+  name  = "admin_iam_user_change_password_policy"
+  group = aws_iam_group.admin_group_test.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "iam:ChangePassword"
+                ],
+                "Resource": [
+                    "arn:aws:iam::*:user/*"
+                ]
+            },
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "iam:GetAccountPasswordPolicy"
+                ],
+                "Resource": "*"
             }
         ]
     })
