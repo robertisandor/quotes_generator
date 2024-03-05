8676b2259384493929b214d7ba82731ab7f4c06b
Robert Sandor <robert.i.sandor@gmail.com>
2024-03-04 18:32:23 -0800

    Add identity provider and iam role

--- terraform/main.tf
+++ terraform/main.tf
@@ -392,7 +392,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_iam_openid_connect_provider" "github_actions" {
-  url = "token.actions.githubusercontent.com"
+  url = "https://token.actions.githubusercontent.com"
 
   client_id_list = [
     "sts.amazonaws.com"
