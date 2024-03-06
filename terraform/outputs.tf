0d85b8f5e1020b63b42604d74c4c524165dd52f4
Robert Sandor <robert.i.sandor@gmail.com>
2024-03-05 19:12:41 -0800

    Update terraform outputs for debugging

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,17 +6,14 @@ output "ec2_hostname" {
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.quotes_generator.address
-  sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.quotes_generator.username
-  sensitive   = true
 }
 
 output "rds_password" {
   description = "RDS instance root password"
-  value       = aws_db_instance.quotes_generator.password
-  sensitive   = true
+  value       = aws_db_instance.quotes_generator.password  
 }
