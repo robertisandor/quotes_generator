0fc42b0ed5ce7a786163ca421ac60060c1136c12
Robert Sandor <robert.i.sandor@gmail.com>
2024-03-07 19:51:23 -0800

    Fix typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,7 +94,7 @@ resource "aws_db_instance" "quotes_generator" {
   publicly_accessible    = false
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_ec2_1.id] 
-  db_subnet_group_name   = aws_subnet.quotes_1
+  db_subnet_group_name   = aws_subnet.quotes_1.name
 }
 
 resource "aws_db_parameter_group" "quotes_generator" {
