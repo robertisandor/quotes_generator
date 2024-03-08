fe3370fe9f51548a734a491fb9ee48517600710a
Robert Sandor <robert.i.sandor@gmail.com>
2024-03-07 23:23:33 -0800

    Tinker with network interfaces

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,17 +129,6 @@ resource "aws_route_table" "quotes_route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.quotes_gateway.id
   }
-
-  # since this is exactly the route AWS will create, the route will be adopted
-  route {
-    cidr_block = "172.31.16.0/20"
-    network_interface_id = aws_network_interface.ec2_network_interface.id
-  }
-
-  route {
-    cidr_block = "172.31.0.0/20"
-    network_interface_id = aws_network_interface.rds_network_interface.id
-  }
 }
 
 resource "aws_route_table_association" "quotes_2" {
@@ -251,32 +240,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_tls_ipv6" {
   from_port         = 5432
   ip_protocol       = "tcp"
   to_port           = 5432
-}
-
-resource "aws_security_group" "default" {
-  name        = "default_sg"
-  vpc_id      = aws_vpc.quotes_main.id
-  description = "default VPC security group"
-}
-
-resource "aws_security_group_rule" "allow_all_traffic_ingress" {
-  security_group_id = aws_security_group.default.id
-  description       = "Rule to allow all ingress traffic"
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 0
-  protocol          = -1
-  to_port           = 0
-  type              = "ingress"
-}
-
-resource "aws_security_group_rule" "allow_all_traffic_egress" {
-  security_group_id = aws_security_group.default.id
-  description       = "Rule to allow all egress traffic"
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 0
-  protocol          = -1
-  to_port           = 0
-  type              = "egress"
 }
 
 resource "aws_security_group" "primary" {
