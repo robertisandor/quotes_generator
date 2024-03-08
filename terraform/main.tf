4cb7a36f67a4f483476d12c77ce1d5ccfb813f03
Robert Sandor <robert.i.sandor@gmail.com>
2024-03-07 19:49:17 -0800

    Update vpc and security groups of ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -365,7 +365,8 @@ resource "aws_instance" "web" {
     ami                     = data.aws_ami.ubuntu.id
     instance_type           = "t2.micro"
     key_name                = aws_key_pair.apiuser.key_name
-    vpc_security_groups_ids = [aws_security_group.ec2_rds_1.id, aws_security_group.default.id]
+    vpc_security_group_ids  = [aws_security_group.ec2_rds_1.id, aws_security_group.default.id]
+    subnet_id               = aws_subnet.quotes_1
 
     user_data = <<-EOL
     #!/bin/bash -xe
