8ea8293eb62f96e8c5579d2605b7dcd715032b27
Robert Sandor <robert.i.sandor@gmail.com>
2024-03-08 23:14:39 -0800

    Update network components in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -263,7 +263,7 @@ resource "aws_security_group_rule" "allow_ssh_egress" {
   description       = "Rule to allow SSH connections from EC2 to reach internet"
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 22
-  ip_protocol       = "tcp"
+  protocol          = "tcp"
   to_port           = 22
   type              = "egress"
 }
