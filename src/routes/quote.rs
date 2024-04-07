8c11c3e24a0535fe051de66ffef4f50f8630046c
Robert Sandor <robert.i.sandor@gmail.com>
2024-04-06 22:16:52 -0700

    Use parameterized query for quote creation

--- src/routes/quote.rs
+++ src/routes/quote.rs
@@ -1,23 +1,21 @@
 use axum::Json;
-use diesel::RunQueryDsl;
-use diesel::sql_query;
+use diesel::{RunQueryDsl, sql_query, sql_types::Text};
 use log::info;
 use serde_json::json;
 
 use crate::services::NewQuote;
 use crate::services::establish_connection_pg;
 
+const insert_query: &str = r#"INSERT INTO quotes (text, speaker) VALUES ($1, $2);"#;
+
 pub async fn create_quote(quote: Json<NewQuote>) -> Json<String> {
     info!("Creating db connection for /api/quote");
     let connection = &mut establish_connection_pg();
     info!("Created db connection for /api/quote");
-    let query_string = format! (r#"
-        INSERT INTO quotes
-            (text, speaker)
-        VALUES
-            ('{}', '{}');
-    "#, quote.text.to_string(), quote.speaker.to_string());
-    sql_query(query_string)
+    
+    sql_query(insert_query)
+        .bind::<Text, _>(quote.text.to_string())
+        .bind::<Text, _>(quote.speaker.to_string())
         .execute(connection)
         .expect("Error saving new post");
     info!("Ran query for /api/quote");
