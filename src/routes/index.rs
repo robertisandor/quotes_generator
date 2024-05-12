c33a0a8628a9c8d61cabf1d945d6106b67fb466a
Robert Sandor <robert.i.sandor@gmail.com>
2024-05-12 00:41:20 -0700

    Set header fields that don't cause issues in index

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -5,24 +5,12 @@ use axum::{
 };
 
 pub async fn index(headers: HeaderMap) -> Json<&'static str> {
-    // event!(Level::INFO, host = headers.get("host").unwrap().to_str().unwrap() 
-    //                   , user_agent = headers.get("user-agent").unwrap().to_str().unwrap()
-    //                   , accept = headers.get("accept").unwrap().to_str().unwrap()
-    //                   , accept_language = headers.get("accept-language").unwrap().to_str().unwrap()
-    //                   , accept_encoding = headers.get("accept-encoding").unwrap().to_str().unwrap()
-    //                   , upgrade_insecure_requests = headers.get("upgrade-insecure-requests").unwrap().to_str().unwrap()
-    //                   , sec_fetch_dest = headers.get("sec-fetch-dest").unwrap().to_str().unwrap()
-    //                   , sec_fetch_mode = headers.get("sec-fetch-mode").unwrap().to_str().unwrap()
-    //                   , sec_fetch_site = headers.get("sec-fetch-site").unwrap().to_str().unwrap()
-    //                   , sec_fetch_user = headers.get("sec-fetch-user").unwrap().to_str().unwrap());
     event!(Level::INFO, host = headers.get("host").unwrap().to_str().unwrap() 
                       , user_agent = headers.get("user-agent").unwrap().to_str().unwrap()
                       , accept = headers.get("accept").unwrap().to_str().unwrap()
                       , accept_language = headers.get("accept-language").unwrap().to_str().unwrap()
                       , accept_encoding = headers.get("accept-encoding").unwrap().to_str().unwrap()
-                      , upgrade_insecure_requests = headers.get("upgrade-insecure-requests").unwrap().to_str().unwrap()
-                      , sec_fetch_dest = headers.get("sec-fetch-dest").unwrap().to_str().unwrap());
-                    //   , sec_fetch_mode = headers.get("sec-fetch-mode").unwrap().to_str().unwrap());
+                      , upgrade_insecure_requests = headers.get("upgrade-insecure-requests").unwrap().to_str().unwrap());
     Json(r#"{"status": "good"}"#)
 }
 
