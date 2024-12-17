2d5e57663a30b618f51a47c802437643e14bee23
Robert Sandor <robert.i.sandor@gmail.com>
2024-12-17 15:38:13 -0800

    Temporarily comment out terraform for debugging

--- terraform/main.tf
+++ terraform/main.tf
@@ -444,6 +444,7 @@ data "aws_ami" "ubuntu" {
     owners = ["099720109477"] # Canonical
 }
 
+/*
 resource "aws_instance" "web" {
     ami                     = data.aws_ami.ubuntu.id
     instance_type           = "t2.micro"
@@ -478,4 +479,4 @@ resource "aws_instance" "web" {
         Name = "QuotesApiWeb"
     }
 }
-
+*/
