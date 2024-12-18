789f28a161f2e31beed59a56a109d3a05f2ead3b
Robert Sandor <robert.i.sandor@gmail.com>
2024-12-17 18:17:56 -0800

    Update ingress rules to allow my IP to access and add comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -298,19 +298,23 @@ resource "aws_security_group_rule" "allow_https_egress" {
   type              = "egress"
 }
 
+# an ingress rule to allow my personal IP to access this for development
+# can be removed/changed once I open it up to more people
 resource "aws_vpc_security_group_ingress_rule" "allow_internet_access" {
   security_group_id = aws_security_group.primary.id
   description       = "Rule to allow connections from internet to reach EC2"
-  cidr_ipv4         = "18.118.34.83/32"
+  cidr_ipv4         = "98.45.195.5/32"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
 }
 
+# an ingress rule to allow my personal IP to access this for development
+# can be removed/changed once I open it up to more people
 resource "aws_vpc_security_group_ingress_rule" "allow_internet_access_port_8000" {
   security_group_id = aws_security_group.primary.id
   description       = "Rule to allow connections from internet to reach EC2"
-  cidr_ipv4         = "18.118.34.83/32"
+  cidr_ipv4         = "98.45.195.5/32"
   from_port         = 8000
   ip_protocol       = "tcp"
   to_port           = 8000
