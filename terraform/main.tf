fee7c15a5e2398057d46863e29676fc98a5baf38
Robert Sandor <robert.i.sandor@gmail.com>
2025-04-26 20:20:11 -0700

    Update terraform s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
     region         = "us-east-2"
     encrypt        = true
     kms_key_id     = "alias/quotes-generator-terraform-bucket-key"
-    dynamodb_table = "terraform-state"
+    use_lockfile   = true
   }
 }
 
