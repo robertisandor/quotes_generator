debe40ca72d66e7b4a603e4e9dcbabeae702daf1
Robert Sandor <robert.i.sandor@gmail.com>
2025-04-30 20:25:13 -0700

    Read target_stores.json from s3

--- ingestion/src/target_webscraper.py
+++ ingestion/src/target_webscraper.py
@@ -2,12 +2,13 @@ import requests
 import datetime 
 import json 
 import boto3
+from boto3 import Session
 from random_header_generator import HeaderGenerator
 
 generator = HeaderGenerator()
 headers = generator()
 
-env = 'prod'
+env = 'production'
 s3_bucket_name = f'inflation-price-tracker-{env}'
 
 ingestion_datetime = datetime.datetime.utcnow()
@@ -16,15 +17,25 @@ s3_key_prefix = f'ingestion/raw/year={ingestion_datetime.year}/month={ingestion_
 # TODO: see if I can populate tcins for categories in a better way than manually
 tcins = ['86918585', '87956594', '86918586', '14901126']
 
+session = Session()
+credentials = session.get_credentials()
+current_credentials = credentials.get_frozen_credentials()
+
+s3 = boto3.client('s3', 
+                  aws_access_key_id=current_credentials.access_key, 
+                  aws_secret_access_key=current_credentials.secret_key)
+
 # read existing Target stores file
 stores = {}
-stores_filename = 'target_stores.json'
-with open(stores_filename) as f:
-    for line in f:
-        stores = stores | json.loads(line)
+stores_s3_filepath = 'ingestion/raw/year=2025/month=5/day=1/country=US/target_stores.json'
+target_stores_data = s3.get_object(Bucket=s3_bucket_name, Key=stores_s3_filepath)
+target_stores_json = target_stores_data['Body'].readlines()
+
+for line in target_stores_json:
+    stores = stores | json.loads(line)
 
 api_urls = []
-for store_id in list(stores.keys())[:5]:
+for store_id in list(stores.keys())[:3]:
     for tcin in tcins:
         api_urls.append({'store': stores[store_id], 'tcin': tcin, 'url': f'https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&tcin={tcin}&is_bot=false&store_id={store_id}&pricing_store_id={store_id}&has_pricing_store_id=true&has_financing_options=true&include_obsolete=true&visitor_id=0196569BE874020187481D35502016A6&skip_personalized=true&skip_variation_hierarchy=true&channel=WEB&page=%2Fp%2FA-86918585'})
 
@@ -84,8 +95,6 @@ for country in products:
         filenames[filename] = s3_key_prefix + f'country={country}/state={state}/{filename}'
 print(filenames)
 
-
-# # s3 = boto3.client('s3')
-# # s3_key = s3_key_prefix + filename
-# # with open(filename, 'rb') as s3_file:
-# #     s3.upload_fileobj(products, s3_bucket_name, s3_key)
+for filename in filenames.keys():
+    s3_key = filenames[filename]
+    response = s3.upload_file(filename, s3_bucket_name, s3_key)
