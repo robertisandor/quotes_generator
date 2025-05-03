20ba278ae4283ec522a6922b591f3536e472b563
Robert Sandor <robert.i.sandor@gmail.com>
2025-05-02 21:55:51 -0700

    Update Cloudwatch log group for lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -154,7 +154,6 @@ data "aws_iam_policy_document" "assume_role" {
 
 resource "aws_cloudwatch_log_group" "target_webscraper_api" {
   name = "/aws/lambda/target_webscraper_api"
-  log_group_class = "STANDARD"
   retention_in_days = 0
 }
 
