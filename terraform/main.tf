0046490fa411ae1e3dfd6351834b2c0b513f8341
Robert Sandor <robert.i.sandor@gmail.com>
2025-08-22 20:56:16 -0700

    Update transformer lambda to python3.10 runtime because terraform doesn't support 3.13

--- terraform/main.tf
+++ terraform/main.tf
@@ -311,7 +311,7 @@ resource "aws_lambda_function" "target_store_location_transformer" {
   architectures     = ["x86_64"]
 
   source_code_hash  = data.archive_file.lambda.output_base64sha256
-  runtime           = "python3.13"
+  runtime           = "python3.10"
 }
 
 resource "aws_iam_role" "target_store_location_transformer_role" {
